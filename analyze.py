9e6ac29bc53da1b2277ee592e7b1db227a7a5978
fosdos <fosterclarksonwilliams@gmail.com>
2017-12-19 23:15:14 -0500

    beta

--- analyze.py
+++ analyze.py
@@ -6,17 +6,30 @@ for line in data:
   nums.append(int(line))
 flag = False
 print "======================================================"
-print "-------------------LITECOIN---------------------------"
-print ("Trend in past 3 weeks has been: " + str(nums[0]) + ", " + str(nums[1]) + ", " + str(nums[2]) + ".")
-print " "
-if(nums[0]*2 < nums[1]):
-  if(nums[1]*2 < nums[2]):
-    print sys.argv[1] + " currently is trending with double 100% increases in the past 2 weeks"
-    print " " 
-    flag = True
-if flag == False:
-  print "nah fam you good, " + sys.argv[1] + " aint that hot rn"
+print "-------------------" + sys.argv[1] + "---------------------------"
+try:
+  print ("Trend in past 3 weeks has been: " + str(nums[0]) + ", " + str(nums[1]) + ", " + str(nums[2]) + ".")
+  print " "
+  if(nums[0]*2 < nums[1]):
+    if(nums[1]*2 < nums[2]):
+      print sys.argv[1] + " currently is trending with double 100% increases in the past 2 weeks"
+      print " " 
+      flag = True
+  if flag == False:
+    print "nah fam you good, " + sys.argv[1] + " aint that hot rn"
+except:
+  print "Not enough relevant data for " + sys.argv[1] 
   print " " 
 print "------------------------------------------------------"
 print "======================================================"
+print "..."
+print "..."
+print "..."
+print "..."
+print "..."
+print "..."
+print "..."
+print "..."
+print "..."
+print "..."
 
--- cloutCoin.py
+++ cloutCoin.py
@@ -1,14 +1,41 @@
 from os import system
 from time import sleep
+from string import printable
 
 zzz = open('currency.txt','r')
 currencies = []
 currency = 0;
 for line in zzz:
   currencies.append(line)
-
+system('rm finished')
 for x in range(len(currencies)):
   currency = currencies[x]
-  currency.lstrip()
-  command = 'python trend_data.py purchase ' + currency + ' > ' 
-  print command
+  currency = currency.strip('\n')
+  file1 = currency + '_data1.txt'
+  file2 = currency + '_data2.txt'
+  file3 = currency + '_data3.txt'
+  file4 = currency + '_data4.txt'
+  command1 = 'python trend_data.py purchase ' + currency + ' > ' + file1
+  command2 = 'tail ' + file1 + ' > ' + file2
+  command3 = 'rm ' + file1
+  command4 = 'head -8 ' + file2 + ' > '  + file3
+  command5 = 'rm ' + file2
+  command6 = 'tail -3 ' + file3+  ' > ' + file4
+  command7 = 'rm ' + file3
+  command8 = 'python data_clean.py ' + file4 + ' > ' + currency
+  command9 = 'python analyze.py ' + currency + ' >> finished'
+  command10 = 'rm ' + file4
+  command11 = 'rm ' + currency
+  system(command1)
+  system(command2)
+  system(command3)
+  system(command4)
+  system(command5)
+  system(command6)
+  system(command7)
+  system(command8)
+  system(command9)
+  system(command10)
+  system(command11)
+
+
